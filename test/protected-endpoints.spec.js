e7c998e5c85396e8087eca5a7fa4884e2c9a336d
stephenschab <sschab689@gmail.com>
2019-12-19 09:49:04 -0700

    added requireAuth middleware

---
+++ test/protected-endpoints.spec.js
@@ -0,0 +1,21 @@
+const knex = require('knex')
+const app = require('../src/app')
+const helpers = require('./test-helpers')
+
+describe.only('Protected endpoints', () => {
+  let db
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
+
+  before('cleanup', () => helpers.cleanTables(db))
+
+  afterEach('cleanup', () => helpers.cleanTables(db))
+})
